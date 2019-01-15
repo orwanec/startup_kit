596690194a84d6b1a48515caa179e50fc1f7d38d
Pavlenko <denys.pavlenko@wirecard.lan>
2019-01-15 17:54:13 +0100

    Fixed the BUG with applying the CSS after webpack compilation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const numeral = require('numeral');
 require('./index.css');
+
+const numeral = require('numeral');
 
 const courseValue = numeral(1000).format('$0,0.00');
 console.log(`I would pay ${courseValue} for this awesome course!`);
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -23,9 +24,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: {
-          loader: 'css-loader'
-        }
+        use:['style-loader','css-loader']
       }
     ]
   }
